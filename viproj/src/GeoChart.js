d964ad5e8d527a0d5ac21d0ed1126670384b556d
Andre <andresmribeiro@gmail.com>
2021-04-10 16:43:59 +0100

    here u go :D

--- viproj/src/GeoChart.js
+++ viproj/src/GeoChart.js
@@ -1,12 +1,14 @@
 // install (please make sure versions match peerDependencies)
 // yarn add @nivo/core @nivo/geo
 import React from 'react';
-import { ResponsiveChoropleth } from '@nivo/geo'
+import {ResponsiveChoroplethCanvas} from '@nivo/geo'
 
-class GeoChart  extends React.Component{
+const worldCountries = require('./worldCountries.json');
 
-  render(){
-    const data =[
+class GeoChart extends React.Component {
+
+  render() {
+    const data = [
       {
         "id": "AFG",
         "value": 940751
@@ -580,50 +582,38 @@ class GeoChart  extends React.Component{
         "value": 676993
       }]
     return (
-      <div>
-        <ResponsiveChoropleth
+      <div style={{width: '900px', height: '500px'}}>
+        <ResponsiveChoroplethCanvas
           data={data}
-          features="/* please have a look at the description for usage */"
-          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-          colors="nivo"
-          domain={[ 0, 1000000 ]}
-          unknownColor="#666666"
+          features={worldCountries.features}
+          margin={{top: 0, right: 0, bottom: 0, left: 0}}
+          colors="RdBu"
+          domain={[0, 1000000]}
+          unknownColor="#101b42"
           label="properties.name"
           valueFormat=".2s"
-          projectionScale={366}
-          projectionTranslation={[ 0.15, 0.05 ]}
-          projectionRotation={[ 0, 0, 0 ]}
+          projectionTranslation={[0.5, 0.5]}
+          projectionRotation={[0, 0, 0]}
           enableGraticule={true}
-          graticuleLineColor="#dddddd"
+          graticuleLineColor="rgba(0, 0, 0, .2)"
           borderWidth={0.5}
-          borderColor="#152538"
+          borderColor="#101b42"
           legends={[
-              {
-                  anchor: 'bottom-left',
-                  direction: 'column',
-                  justify: true,
-                  translateX: 20,
-                  translateY: -100,
-                  itemsSpacing: 0,
-                  itemWidth: 94,
-                  itemHeight: 18,
-                  itemDirection: 'left-to-right',
-                  itemTextColor: '#444444',
-                  itemOpacity: 0.85,
-                  symbolSize: 18,
-                  effects: [
-                      {
-                          on: 'hover',
-                          style: {
-                              itemTextColor: '#000000',
-                              itemOpacity: 1
-                          }
-                      }
-                  ]
-              }
+            {
+              anchor: 'bottom-left',
+              direction: 'column',
+              justify: true,
+              translateX: 20,
+              translateY: -60,
+              itemsSpacing: 0,
+              itemWidth: 92,
+              itemHeight: 18,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 18
+            }
           ]}
-      />
-
+        />
       </div>
     )
   }
