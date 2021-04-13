2aef07a461143b0c3a61e9d373f8a5d35725dc87
ednamartins <Eliedygaspar@gmail.com>
2021-04-13 18:06:54 +0300

    components

--- viproj/src/App.js
+++ viproj/src/App.js
@@ -1,30 +1,16 @@
-import React from 'react'
-import Header from './Header';
-import LeftSide from './LeftSide';
-import RightSide from './RightSide';
+import React from 'react';
+import PrincipalComp from './PrincipalComp';
 
 class App extends React.Component{
   render(){
-    const colums = {
-      backgroundColor: "DodgerBlue",
-      float:"left",
-      width: "50%",
-      height:"100%",
-    };
     const app = {
       height: '500px',
-      width: '900px',
+      width: '1200px',
       fontFamily: "Arial"
     };
       return(
           <div style={app}>
-             <Header />
-            <div style={colums} > 
-              <LeftSide />
-            </div>
-            <div style={colums} >
-             <RightSide />
-             </div>
+           <PrincipalComp />
           </div>
       );
   }
